303c17d75a3ef2365a120a170622f6e960dff548
qaovxtazypdl <x67zhao@uwaterloo.ca>
2016-02-25 15:45:46 -0500

    initial commit a5

---
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/qaovxtazypdl/assignment5/Q7.scala
@@ -0,0 +1,33 @@
+package ca.uwaterloo.cs.bigdata2016w.qaovxtazypdl.assignment5
+
+import util.Tokenizer
+import util._
+import scala.collection.mutable.{HashMap}
+
+import org.apache.log4j._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+import org.rogach.scallop._
+
+class ConfQ7(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
+  mainOptions = Seq(input, date)
+  val input = opt[String](descr = "input path", required = true)
+  val date = opt[String](descr = "date", required = true)
+}
+
+object Q7 {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+    val args = new ConfQ7(argv)
+
+    log.info("Input: " + args.input())
+    log.info("Date: " + args.date())
+
+    val conf = new SparkConf().setAppName("A5Q7")
+    val sc = new SparkContext(conf)
+
+    val textFile = sc.textFile(args.input())
+  }
+}
