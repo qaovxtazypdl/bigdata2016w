ef943ea87ab634e05cf159a178abb2bc4bf98039
qaovxtazypdl <x67zhao@uwaterloo.ca>
2016-03-02 12:16:44 -0500

    ?

--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/qaovxtazypdl/assignment5/Q6.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/qaovxtazypdl/assignment5/Q6.scala
@@ -53,96 +53,19 @@ object Q6 {
           val tokens = line.split('|')
           val extended = tokens(5).toFloat
           val discount = tokens(6).toFloat
-          ((tokens(8), tokens(9)), (tokens(4).toFloat, extended, extended*(1-discount), extended*(1-discount)*(1+tokens(7).toFloat), discount))
+          ((tokens(8), tokens(9)), Array(tokens(4).toFloat, extended, extended*(1-discount), extended*(1-discount)*(1+tokens(7).toFloat), discount, 1))
         })
-        //group by the aggregate key
-        .groupByKey()
-        //do the aggregation
-        .map(keyValuePair => {
-          val count = keyValuePair._2.size
-          val sums = Array(0.0f,0.0f,0.0f,0.0f,0.0f) //quantity extended disc charge discount
-/*
-          val arr = keyValuePair._2.toIterator
-          for (i <- count%8 until count by 8) {
-            val l1 = arr.next()
-            val l2 = arr.next()
-            val l3 = arr.next()
-            val l4 = arr.next()
-            val l5 = arr.next()
-            val l6 = arr.next()
-            val l7 = arr.next()
-            val l8 = arr.next()
-
-
-            sums(0) += l1._1
-            sums(1) += l1._2
-            sums(2) += l1._3
-            sums(3) += l1._4
-            sums(4) += l1._5
-
-            sums(0) += l2._1
-            sums(1) += l2._2
-            sums(2) += l2._3
-            sums(3) += l2._4
-            sums(4) += l2._5
-
-            sums(0) += l3._1
-            sums(1) += l3._2
-            sums(2) += l3._3
-            sums(3) += l3._4
-            sums(4) += l3._5
-
-            sums(0) += l4._1
-            sums(1) += l4._2
-            sums(2) += l4._3
-            sums(3) += l4._4
-            sums(4) += l4._5
-
-            sums(0) += l5._1
-            sums(1) += l5._2
-            sums(2) += l5._3
-            sums(3) += l5._4
-            sums(4) += l5._5
-
-            sums(0) += l6._1
-            sums(1) += l6._2
-            sums(2) += l6._3
-            sums(3) += l6._4
-            sums(4) += l6._5
-
-            sums(0) += l7._1
-            sums(1) += l7._2
-            sums(2) += l7._3
-            sums(3) += l7._4
-            sums(4) += l7._5
-
-            sums(0) += l8._1
-            sums(1) += l8._2
-            sums(2) += l8._3
-            sums(3) += l8._4
-            sums(4) += l8._5
-
-          }
-          while (arr.hasNext) {
-            val el = arr.next()
-            sums(0) += el._1
-            sums(1) += el._2
-            sums(2) += el._3
-            sums(3) += el._4
-            sums(4) += el._5
-          }
-*/
-
-//(running time is actually about the same compared to "vectorized"... maybe because row store)
-          keyValuePair._2.foreach(tuple => {
-            sums(0) += tuple._1
-            sums(1) += tuple._2
-            sums(2) += tuple._3
-            sums(3) += tuple._4
-            sums(4) += tuple._5
-          })
-
-          (keyValuePair._1._1, keyValuePair._1._2, sums(0), sums(1), sums(2), sums(3), sums(0)/count, sums(1)/count, sums(4)/count, count)
+        .reduceByKey((acc, lst) => {
+          acc(0) += lst(0)
+          acc(1) += lst(1)
+          acc(2) += lst(2)
+          acc(3) += lst(3)
+          acc(4) += lst(4)
+          acc(5) += lst(5)
+          acc
+        })
+        .map(v => {
+          (v._1._1, v._1._2, v._2(0), v._2(1), v._2(2), v._2(3), v._2(0)/v._2(5), v._2(1)/v._2(5), v._2(4)/v._2(5), v._2(5))
         })
         .collect()
         .foreach(println)
